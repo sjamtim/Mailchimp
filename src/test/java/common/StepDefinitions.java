d92c204298b2c684eae8a9f81a6721d784a24aa9
Tim <timand1991@gmail.com>
2021-03-23 15:25:29 +0100

    Första commiten

---
+++ src/test/java/common/StepDefinitions.java
@@ -0,0 +1,35 @@
+package common;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StepDefinitions {
+
+    WebDriver driver;
+
+    @Given("User have navigated to the website")
+    public void userHaveNavigatedToTheWebsite() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://login.mailchimp.com/signup/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+        //waiter.until(ExpectedConditions.invisibilityOfElementLocated(By.className("loading-mask")));
+    }
+    @Given("Register {int} random user")
+    public void registerRandomUser(int randomUser) throws InterruptedException {
+        WebElement searchBoxEmail = driver.findElement(By.cssSelector("input[type=email]"));
+        searchBoxEmail.sendKeys(Integer.toString(randomUser));
+        searchBoxEmail.sendKeys(Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+}
+
---
+++ src/test/resources/Features/registration.feature
@@ -0,0 +1,17 @@
+Feature: Mailchimp
+  Try out the registration!
+
+  Background:
+    Given User have navigated to the website
+
+  Scenario: Register user
+    Given Register 1 random user
+
+  Scenario: Register user with long username (100 characters+)
+
+
+  Scenario: Register user with already taken username
+
+
+  Scenario: Register user without email
+
